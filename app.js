1aa230bafd4755007cff755341f5a49493927dbb
Corey Willinger <corey.willinger@gmail.com>
2022-05-19 09:42:10 -0400

    updated custome lists add/delete.  Cleaned up code

--- app.js
+++ app.js
@@ -1,94 +1,133 @@
 
 /* -------node.js app boilerplate--------*/
 
-//Server setup
+//npm modules
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
+const _ = require("lodash");
 const mongoose = require("mongoose");
+
+//configure Express server
+const app = express();
+const PORT = 3000;                                      //3000 for testing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-const PORT = 3000;
 
-/* ------------Database Setup ------------*/
-const DB_PORT = 27017;
-const DB_URI = "mongodb://localhost:" + DB_PORT + "/todoDB";
-mongoose.connect(DB_URI);
-
-//create schema and model
-const taskSchema = new mongoose.Schema({
-    name: String
-});
-const Task = mongoose.model("Task", taskSchema);
-var currentTasks = [];
-
-/* --------------Server Logic --------*/
-
-//Start server listing on port
+//Start server listing
 app.listen(PORT, function () {
     console.log("Web server running on port " + PORT);
 });
 
-app.get("/", response);
-app.post("/", post_root);
-app.post("/delete", post_delete);
+/* ---------------Database Setup ------------*/
+const DB_PORT = 27017;
+const DB_URI = "mongodb://localhost:" + DB_PORT + "/todoDB";
+mongoose.connect(DB_URI);
+
+//create task schema and model
+const taskSchema = new mongoose.Schema({name: String });
+const Task = mongoose.model("Task", taskSchema);
+
+//create list schema and model
+const listSchema = new mongoose.Schema({listName: String, tasks: [taskSchema] });
+const List = mongoose.model("List", listSchema);
+
+/* --------------------Routes ---------------*/
+
+app.get("/", function (req, res) {
+    Task.find({}, function (err, data) {
+        if (data.length == 0) {
+            console.log("No Task Data Found");
+            const task = new Task({name: "Update tasks"});
+
+            Task.insertMany(task, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Created default item");
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render('list', { date: getDayOfWeek(), title: "Today", tasks: data });
+        }
+    });
+});
+
+app.post("/", function (req, res) {
+    const itemName = req.body.item;                     //req.body comes from the form submission
+    const listName = req.body.list;
+    const task = new Task({name: itemName});
+
+    if (listName === "Today") {                         //if list is "Today" redirect to root
+        task.save();
+        res.redirect("/");
+    } else {                                            //if list is custom redirect back to custom
+        List.findOne({ name: listName }, function (err, data) {
+            data.tasks.push(task);
+            data.save();
+            res.redirect("/" + listName)
+        });
+    }
+});
+
+app.get("/:listName", function (req, res) {
+    const listName = _.capitalize(req.params.listName);                       //req.params comes from the url
+    
+    List.findOne({ name: listName }, function (err, data) {
+        if (!err) {
+            if (!data) { 
+                //create default item and reload custom list           
+                const task = new Task({name: "Update tasks"});
+                const list = new List({name: listName, tasks:[task] });
+                list.save();
+                res.redirect("/" + listName);
+            } else {
+                res.render('list', { date: getDayOfWeek(), title: listName, tasks: data.tasks });
+            }
+        } else {                                    
+            console.log(err);
+        }
+    })
+});
+
+app.post("/delete", function (req, res) {
+    const listName = req.body.listName;             
+    const itemId = req.body.checkbox;
+
+    if (listName === "Today") {                             //delete from default list and redirect to root
+        Task.findByIdAndRemove(itemId, function (err) {
+            if (!err) {
+                console.log("Deleted item: " + itemId);
+                res.redirect("/");
+            } else {                                    
+                console.log(err);
+            }
+        });
+    } else {                                                //delete from custom list and redirect to custome page
+        List.findOneAndUpdate(
+            { name: listName },                             //Condition to find
+            { $pull: { tasks: { _id: itemId } } },          //MongoDb $pull function
+            function (err, data) {
+                if (!err) {
+                    res.redirect("/" + listName);
+                }
+            }
+        );
+    }
+});
+
 app.get("/about", function (req, res) {
     res.render("about");
 });
 
-//Response function for root dir
-function response(req, res) {
+/*-------------Server functions-------------*/
+function getDayOfWeek() {
     var date_opts = {
         weekday: "long",
         day: 'numeric',
         month: 'long'
     };
     var day_of_wk = new Date().toLocaleDateString("en-US", date_opts);
-
-    Task.find({}, function (err, data) {
-        if (data.length == 0) {
-            currentTasks = addDefaultTask();
-            res.redirect("/");
-        } else {
-            currentTasks = data;
-            res.render('list', { day_of_wk: day_of_wk, listTitle: "Today", tasks: currentTasks });
-        }
-    });
-}
-
-//Post function for root dir
-function post_root(req, res) {
-    let new_item = req.body.item;
-    const task = new Task({
-        name: new_item
-    });
-    task.save();
-    res.redirect("/");
-}
-
-//handle item deletions
-function post_delete(req, res) {
-    Task.findByIdAndRemove(req.body.checkbox, function(err){
-        if(err){
-            console.log(err);
-        }else{
-            console.log("Deleted item: " + req.body.checkbox);
-            res.redirect("/");
-        }
-    })
-}
-
-function addDefaultTask(){
-    const defaultTask = new Task({
-        name: "Update tasks"
-    });
-
-    Task.insertMany(defaultTask, function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Created default item");
-        }
-    });
+    return day_of_wk;
 }
